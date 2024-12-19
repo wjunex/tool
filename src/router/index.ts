7567e8e9abf24dc39e8b331856a8755922f01382
wangjun <wangjun@guishangyi.cn>
2024-12-19 14:02:50 +0800

    fix: 网站标题优化

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,29 +1,51 @@
 // src/router/index.ts
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+
+declare module "vue-router" {
+  interface RouteMeta {
+    title?: string; // 定义 title 类型
+  }
+}
 
 // 配置路由
 const routes: Array<RouteRecordRaw> = [
   {
-    path: '/',
-    name: 'Index',
-    component: () => import('@/view/index/index.vue'),
+    path: "/",
+    name: "Index",
+    component: () => import("@/view/index/index.vue"),
+    meta: {
+      title: "首页", // 页面标题
+    },
   },
   {
-    path: '/iframe',
-    name: 'Iframe',
-    component: () => import('@/view/iframe/iframe.vue'),
+    path: "/iframe",
+    name: "Iframe",
+    component: () => import("@/view/iframe/iframe.vue"),
+    meta: {
+      title: "iframe",
+    },
   },
   {
-    path: '/test',
-    name: 'Test',
-    component: () => import('@/view/test/test.vue'),
+    path: "/test",
+    name: "Test",
+    component: () => import("@/view/test/test.vue"),
+    meta: {
+      title: "测试",
+    },
   },
-]
+];
 
 // 创建路由实例
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
-})
+});
 
-export default router
+router.beforeEach((to, from, next) => {
+  if (to.meta.title) {
+    document.title = to.meta?.title || "";
+  }
+  next(); 
+});
+
+export default router;
